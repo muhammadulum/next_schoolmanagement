6b258ad18d885b083b261107d3c50e2d0dc59338
muhammadulum <muhammadmulum99@gmail.com>
2025-08-09 01:49:51 +0700

    cutom big calender

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeacherListPage = () => {
+    return (
+        <div className=''>
+            d
+        </div>
+    )
+}
+
+export default TeacherListPage
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,6 +1,25 @@
+'use client'
+import Announcements from "@/components/Announcements"
+import EventCalender from "@/components/EventCalender"
+import BigCalender from "@/components/BigCallender"
+
+
 const Teacherpage = () => {
     return (
-        <div>
+        <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+            {/* left */}
+            <div className="w-full xl:w-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Schedule(Jhon Doe)</h1>
+                    <BigCalender />
+                </div>
+
+            </div>
+            {/* right */}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+
+                <Announcements /></div>
+
 
         </div>
     )
