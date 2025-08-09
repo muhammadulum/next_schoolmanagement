3f947f0a5221285fc1e84c5bcd95cf1eb3701191
muhammadulum <muhammadmulum99@gmail.com>
2025-08-09 23:45:10 +0700

    add list teacher

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,10 +1,24 @@
+import TableSearch from '@/components/TableSearch'
 import React from 'react'
 
 const TeacherListPage = () => {
     return (
-        <div className=''>
-            d
+        <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+            {/* top */}
+            <div className=''>
+                <h1>All Teacher</h1>
+                <div className=''>
+                    <TableSearch />
+                </div>
+            </div>
+            {/* list */}
+            <div className=''> </div>
+            {/* pagination */}
+            <div className="">
+
+            </div>
         </div>
+
     )
 }
 
